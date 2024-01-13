4e8176996008132ab09edf0504d1dd87b9b124d1
Documenter.jl <documenter@juliadocs.github.io>
2024-01-13 21:18:52 +0000

    build based on ad5ffc9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ManifoldFlows","category":"page"},{"location":"#ManifoldFlows","page":"Home","title":"ManifoldFlows","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ManifoldFlows.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ManifoldFlows]","category":"page"}]
+}
